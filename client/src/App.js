863f5bd24681afe0186d14a9bfa1842f5854f5a8
Dev-Spec <topstar.dev814@gmail.com>
2023-07-27 23:59:08 +0800

    initial

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { io } from 'socket.io-client';
+
+const socket = io.connect("http://localhost:4000");
+
+function App() {
+  return (
+   <div>
+    <p>Hello World!</p>
+   </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Hello world"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
